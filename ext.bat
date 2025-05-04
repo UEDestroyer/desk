e11d62018ea5b808bf8a924da975eaa773fa72b5
ӘД <163650738+UEDestroyer@users.noreply.github.com>
2025-05-04 17:06:30 +0500

    Upload file: ext.bat

--- ext.bat
+++ ext.bat
@@ -1,2 +1,5 @@
 @echo off
-code --install-extension ritwickdey.liveserver
+start /b code --install-extension ritwickdey.liveserver
+start /b code --install-extension ms-vscode.cpptools
+start /b code --install-extension pranaygp.vscode-css-peek
+start /b code --install-extension ecmel.vscode-html-css
