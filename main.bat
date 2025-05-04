4632253a5c0ffff327c3fd4e7df3bc957ff33c25
ӘД <163650738+UEDestroyer@users.noreply.github.com>
2025-05-04 16:05:45 +0500

    Upload file: main.bat

--- main.bat
+++ main.bat
@@ -1,2 +1,21 @@
 @echo off
-setx PATH "%PATH%;C:\msys64\mingw64\bin;C:\msys64\mingw64\include"
+
+set shortcut="C:\msys64\mingw64.lnk"
+set startmenu="%APPDATA%\..\"
+copy %shortcut% %startmenu%
+echo eshe
+set startmenu="%APPDATA%\Microsoft\Windows\Start Menu\Programs"
+@echo on
+copy %shortcut% %startmenu%
+@echo off
+echo Готово!
+pause
+taskkill /F /IM explorer.exe & start explorer.exe
+pause
+./ext.bat
+pause
+echo { > "%APPDATA%\Code\User\settings.json"
+echo "files.autoSave": "afterDelay", >> "%APPDATA%\Code\User\settings.json"
+echo "files.autoSaveDelay": 1000 >> "%APPDATA%\Code\User\settings.json"
+echo } >> "%APPDATA%\Code\User\settings.json"
+echo Авто-сохранение включено!
